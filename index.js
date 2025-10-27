01f150153b9c52ff4f41f348b0cd2d0607569286
りょう <136053253+VEDA0013391@users.noreply.github.com>
2025-10-27 05:23:22 +0000

    mongodbaltasへ移行

--- index.js
+++ index.js
@@ -1,57 +1,51 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
-const Database = require('better-sqlite3');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const getLatestVideos = require('./utils/getLatestVideos');
 const basicAuth = require('./utils/basicAuth');
-const autoLink = require('./utils/convertLink')
+const autoLink = require('./utils/convertLink');
+const News = require('./data/news_db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use('/admin', basicAuth);
 
-const db = new Database('data/news.db');
-db.prepare(`
-CREATE TABLE IF NOT EXISTS news (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  title TEXT NOT NULL,
-  body TEXT NOT NULL,
-  date TEXT NOT NULL
-)
-`).run();
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB接続成功'))
+  .catch(err => console.error('MongoDB接続失敗', err));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
 
-function getLatestNews(limit = 5) {
-  return db.prepare('SELECT * FROM news ORDER BY date DESC LIMIT ?').all(limit);
+async function getLatestNews(limit = 10) {
+  return await News.find().sort({ date: -1 }).limit(limit);
 }
 
 app.get('/', async (req, res) => {
   const vtuber = JSON.parse(fs.readFileSync('./data/vtuber_info.json', 'utf8'));
-  
   const videos = await getLatestVideos();
-  const newsRaw = getLatestNews();
+  const newsRaw = await getLatestNews();
 
   const news = newsRaw.map(item => ({
-    ...item,
-    body: autoLink(item.body)
+    ...item._doc,
+    body: autoLink(item.body.replace(/\n/g, '<br>'))
   }));
 
-  vtuber.introduction = vtuber.introduction.replace(/\n/g, "<br>");
+  vtuber.introduction = vtuber.introduction.replace(/\n/g, '<br>');
   vtuber.videos = videos;
   vtuber.news = news;
 
   res.render('index', { vtuber });
 });
 
-app.get('/admin/news', (req, res) => {
-  const news = db.prepare('SELECT * FROM news ORDER BY date DESC').all();
+app.get('/admin/news', async (req, res) => {
+  const news = await News.find().sort({ date: -1 });
   res.render('admin_news_list', { news });
 });
 
@@ -59,24 +53,24 @@ app.get('/admin/news/new', (req, res) => {
   res.render('admin_news_form', { news: null });
 });
 
-app.get('/admin/news/edit/:id', (req, res) => {
-  const news = db.prepare('SELECT * FROM news WHERE id = ?').get(req.params.id);
+app.get('/admin/news/edit/:id', async (req, res) => {
+  const news = await News.findById(req.params.id);
   if (!news) return res.status(404).send('ニュースが見つかりません');
   res.render('admin_news_form', { news });
 });
 
-app.post('/admin/news/save', (req, res) => {
+app.post('/admin/news/save', async (req, res) => {
   const { id, title, body, date } = req.body;
   if (id) {
-    db.prepare('UPDATE news SET title = ?, body = ?, date = ? WHERE id = ?').run(title, body, date, id);
+    await News.findByIdAndUpdate(id, { title, body, date });
   } else {
-    db.prepare('INSERT INTO news (title, body, date) VALUES (?, ?, ?)').run(title, body, date);
+    await News.create({ title, body, date });
   }
   res.redirect('/admin/news');
 });
 
-app.post('/admin/news/delete/:id', (req, res) => {
-  db.prepare('DELETE FROM news WHERE id = ?').run(req.params.id);
+app.post('/admin/news/delete/:id', async (req, res) => {
+  await News.findByIdAndDelete(req.params.id);
   res.redirect('/admin/news');
 });
 
